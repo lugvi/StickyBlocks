23ae59c8e1d4587a8b7b110928f33123bb0f1580
Luka <lukacho_g@yahoo.com>
2018-07-24 13:34:41 +0400

    g

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+	[Range(0,5)]
+	public float minDist;
+	[Range(0,5)]
+	public float perfDist;
+
+	public static GameManager instance;
+	private void Awake() {
+		instance = this;
+	}
+	public Transform Player;
+
+	[Range(1,300)]
+	public float speed=1;
+
+	Camera cam;
+	public List<Transform> movables;
+	// Use this for initialization
+	void Start () {
+		cam = Camera.main;
+		movables.Add(Player);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButton(0))
+		{	
+			Vector3 clickpos = new Vector3(Input.mousePosition.x,Input.mousePosition.y,cam.transform.position.z);
+			MoveObjectsOnX(movables,Camera.main.ScreenToWorldPoint(clickpos).x);
+		}
+		movePlayer();
+	}
+
+	void MoveObjectsOnX(List<Transform> objects,float x)
+	{	
+		foreach(Transform t in objects)
+		{
+			t.transform.position = new Vector3(-x,t.transform.position.y,t.transform.position.z);
+		}
+	}
+
+	void movePlayer()
+	{
+		Player.transform.Translate(Vector3.up*Time.deltaTime*speed);
+	}
+
+	public void Advance(Collider2D duh)
+	{
+		duh.transform.SetParent(movables[0]);
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+
+	private void OnTriggerEnter2D(Collider2D other) {
+		if(IsClose(other.transform.position,this.transform.position)<GameManager.instance.perfDist)
+		{
+			GameManager.instance.Advance(other);
+			Debug.Log("perfect");
+		}
+		else if(IsClose(other.transform.position,this.transform.position)<GameManager.instance.minDist)
+		{
+			GameManager.instance.Advance(other);
+			Debug.Log("გუდ");
+		}
+		else
+			Debug.Log("ბანძ");
+	}
+
+	
+
+	float IsClose(Vector3 a, Vector3 b)
+	{
+		return Mathf.Abs(a.x-b.x);
+	}
+}
