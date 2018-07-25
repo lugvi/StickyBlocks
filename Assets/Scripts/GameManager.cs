e0be7a625ec601faffad4aecf51c103f08db14fa
Luka <lukacho_g@yahoo.com>
2018-07-25 19:47:47 +0400

    alpha

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,54 +2,142 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour {
-	[Range(0,5)]
-	public float minDist;
-	[Range(0,5)]
-	public float perfDist;
+public class GameManager : MonoBehaviour
+{
 
-	public static GameManager instance;
-	private void Awake() {
-		instance = this;
-	}
-	public Transform Player;
+    public static GameManager instance;
+    private void Awake()
+    {
+        instance = this;
+    }
 
-	[Range(1,300)]
-	public float speed=1;
 
-	Camera cam;
-	public List<Transform> movables;
-	// Use this for initialization
-	void Start () {
-		cam = Camera.main;
-		movables.Add(Player);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if(Input.GetMouseButton(0))
-		{	
-			Vector3 clickpos = new Vector3(Input.mousePosition.x,Input.mousePosition.y,cam.transform.position.z);
-			MoveObjectsOnX(movables,Camera.main.ScreenToWorldPoint(clickpos).x);
-		}
-		movePlayer();
-	}
 
-	void MoveObjectsOnX(List<Transform> objects,float x)
-	{	
-		foreach(Transform t in objects)
-		{
-			t.transform.position = new Vector3(-x,t.transform.position.y,t.transform.position.z);
-		}
-	}
+    public delegate void Click();
 
-	void movePlayer()
-	{
-		Player.transform.Translate(Vector3.up*Time.deltaTime*speed);
-	}
+    public event Click OnClick;
 
-	public void Advance(Collider2D duh)
-	{
-		duh.transform.SetParent(movables[0]);
-	}
+    [Range(0, 5)]
+    public float minDist;
+    [Range(0, 5)]
+    public float perfDist;
+
+    [Range(1, 10)]
+    public float speed = 1;
+    public Transform Player;
+
+    public GameObject prefab;
+
+    public Transform board;
+
+    float spawnHeight = 12;
+
+    int direction = 1;
+
+    public List<Transform> startLines;
+
+    public Queue<Transform> path;
+
+    public Transform currentLine;
+    // Use this for initialization
+    void Start()
+    {
+        path = new Queue<Transform>();
+        foreach (Transform t in startLines)
+        {
+            path.Enqueue(t);
+        }
+        currentLine = path.Dequeue();
+        OnClick += () =>
+        {
+            Clicked();
+        };
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PingPong(currentLine);
+        ScrollDown();
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            OnClick();
+        }
+
+    }
+
+
+
+    void SpawnLine(GameObject obj)
+    {
+        Transform a = Instantiate(obj, board).transform;
+        a.localPosition = Vector3.up * spawnHeight;
+        path.Enqueue(a);
+        a.Translate(Vector3.left * Random.Range(-3f, 3f));
+    }
+
+    void PingPong(Transform tr)
+    {
+        if (tr.position.x >= 3)
+            direction = -1;
+        if (tr.position.x <= -3)
+            direction = 1;
+        tr.Translate(Vector3.right * Time.deltaTime * speed * direction);
+    }
+
+    private void Clicked()
+    {
+        float dist = IsClose(currentLine.position, path.Peek().position);
+        if (dist < minDist)
+        {
+            OnHit();
+            if (dist < perfDist)
+            {
+                OnPerfect();
+            }
+        }
+        else
+        {
+            OnMiss();
+        }
+
+    }
+
+    void ScrollDown()
+    {
+        if (currentLine.position.y > 0)
+        {
+            board.Translate(Vector3.down * Time.deltaTime * (currentLine.position.y+1));
+        }
+
+    }
+
+    void OnHit()
+    {
+		currentLine.SetParent(path.Peek());
+        Destroy(currentLine.gameObject, 5);
+        currentLine = path.Dequeue();
+        direction = Random.value < .5f ? 1 : -1;
+        spawnHeight += 2;
+        SpawnLine(prefab);
+
+    }
+    void OnPerfect()
+    {
+        Debug.Log("Perfect");
+    }
+
+    void OnMiss()
+
+    {
+
+    }
+
+
+
+    float IsClose(Vector3 a, Vector3 b)
+    {
+        return Mathf.Abs(a.x - b.x);
+    }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,25 +5,25 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
 
-	private void OnTriggerEnter2D(Collider2D other) {
-		if(IsClose(other.transform.position,this.transform.position)<GameManager.instance.perfDist)
-		{
-			GameManager.instance.Advance(other);
-			Debug.Log("perfect");
-		}
-		else if(IsClose(other.transform.position,this.transform.position)<GameManager.instance.minDist)
-		{
-			GameManager.instance.Advance(other);
-			Debug.Log("გუდ");
-		}
-		else
-			Debug.Log("ბანძ");
-	}
+	// private void OnTriggerEnter2D(Collider2D other) {
+	// 	if(IsClose(other.transform.position,this.transform.position)<GameManager.instance.perfDist)
+	// 	{
+	// 		GameManager.instance.Advance(other);
+	// 		Debug.Log("perfect");
+	// 	}
+	// 	else if(IsClose(other.transform.position,this.transform.position)<GameManager.instance.minDist)
+	// 	{
+	// 		GameManager.instance.Advance(other);
+	// 		Debug.Log("გუდ");
+	// 	}
+	// 	else
+	// 		Debug.Log("ბანძ");
+	// }
 
 	
 
-	float IsClose(Vector3 a, Vector3 b)
-	{
-		return Mathf.Abs(a.x-b.x);
-	}
+	// float IsClose(Vector3 a, Vector3 b)
+	// {
+	// 	return Mathf.Abs(a.x-b.x);
+	// }
 }
