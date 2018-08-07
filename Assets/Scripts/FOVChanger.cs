3cda6c3bc6b02ed48d48b843fe6f01306322eb54
TedX <tedomakharadze95@gmail.com>
2018-08-07 18:23:07 +0400

    FOV

---
+++ Assets/Scripts/FOVChanger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FOVChanger : MonoBehaviour
+{
+
+
+    private Camera cam;
+
+    public float hor;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+        cam.fieldOfView = calcVertivalFOV(hor, cam.aspect);
+
+    }
+
+    private float calcVertivalFOV(float hFOVInDeg, float aspectRatio)
+    {
+        float hFOVInRads = hFOVInDeg * Mathf.Deg2Rad;
+        float vFOVInRads = 2 * Mathf.Atan(Mathf.Tan(hFOVInRads / 2) / aspectRatio);
+        float vFOV = vFOVInRads * Mathf.Rad2Deg;
+        return vFOV;
+    }
+}
